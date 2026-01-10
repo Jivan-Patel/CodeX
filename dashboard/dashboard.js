c0fa5d15444ca87ae8e021cfa3bd175db04731bc
Patel-Jivan <jivangj0@gmail.com>
2026-01-10 10:13:46 +0530

    Dashboard Functionallity Complete

---
+++ dashboard/dashboard.js
@@ -0,0 +1,79 @@
+const seeTransCardDisplay = document.querySelector('.seeTransCard');
+const monthIncDisplay = document.getElementById('monthInc')
+const monthExpDisplay = document.getElementById('monthExp')
+const totalBalanceDisplay = document.getElementById('totalBalance')
+
+var data = [];
+let totalInc = 0;
+let totalExp = 0;
+let monthExp = 0;
+let monthInc = 0;
+let initialBalance = null;
+
+
+function fetchData() {
+    data = JSON.parse(localStorage.getItem('transactions')) || [];
+    initialBalance = Number(localStorage.getItem('initialBalance'));
+    if (!initialBalance) {
+        initialBalance = Number(prompt("Enter Initial Balance"));
+        localStorage.setItem('initialBalance', initialBalance);
+    }
+    
+
+    
+    for (obj of data) {
+        if (obj.incExp == 'expense') {
+            totalExp += Number(obj.amount);
+        }
+        else {
+            totalInc += Number(obj.amount);
+        }
+    }
+    
+    for (let i = data.length - 1; i > data.length - 1 - 5; i--) {
+        displayTrans(data[i]);
+    }
+
+    const now = new Date();
+    const lastMonth = now.getMonth() === 0 ? 11 : now.getMonth() - 1;
+    const lastMonthYear = now.getMonth() === 0 ? now.getFullYear() - 1 : now.getFullYear();
+    const lastMonthData = data.filter(item => {
+        const d = new Date(item.date);
+        return d.getMonth() === lastMonth && d.getFullYear() === lastMonthYear;
+    });
+    for (obj of lastMonthData) {
+        if (obj.incExp == 'expense') {
+            monthExp += Number(obj.amount);
+        }
+        else {
+            monthInc += Number(obj.amount);
+        }
+    }
+    monthIncDisplay.innerHTML = monthInc;
+    monthExpDisplay.innerHTML = monthExp;
+    totalBalanceDisplay.innerHTML = initialBalance + totalInc - totalExp;
+}
+
+function displayTrans(obj) {
+    let div = document.createElement('div');
+    div.classList.add('scard');
+    div.innerHTML = `<div class="scardP1 scardP">
+                        <div class="sCat">${obj.catagory}</div>
+                        <div class="sdate">${obj.date}</div>
+                        <div class="snote">${obj.description}</div>
+                    </div>
+                    <div class="scardP2 scardP">
+                        <div class="sAmt">${(obj.incExp == 'expense') ? '-' : "+"}${obj.amount}</div>
+                        <div class="seeIncExp s${obj.incExp}">${obj.incExp}</div>
+                    </div>`;
+    seeTransCardDisplay.appendChild(div);
+}
+
+function recentTrans() {
+    console.log(data);
+}
+
+
+
+
+fetchData();
